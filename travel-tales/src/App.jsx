030de37b6c57402f9fe894c442e322373ab1593c
AINAN HASSAN <ainanhassan123@gmail.com>
2024-09-16 21:30:18 +0300

    first banner loading

--- travel-tales/src/App.jsx
+++ travel-tales/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from "./components/Header";
+import Banner from './components/welcomeBanner';
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
           <Header></Header>
-      
+           <Banner></Banner>
     
     </>
     )
---
+++ travel-tales/src/components/welcomeBanner.jsx
@@ -0,0 +1,14 @@
+import './styles/Banner.css';
+
+
+function Banner() {
+    return (
+        <section className="welcome-banner">
+          blablbalabalalabballa
+        </section>
+    )
+}
+
+
+
+export default Banner;
