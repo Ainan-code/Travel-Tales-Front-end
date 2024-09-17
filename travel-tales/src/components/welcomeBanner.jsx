3ff580ebd4df8d120b64d5256a208feee68d9bbf
AINAN HASSAN <ainanhassan123@gmail.com>
2024-09-17 09:21:33 +0300

    adding the welcome banner

--- travel-tales/src/components/welcomeBanner.jsx
+++ travel-tales/src/components/welcomeBanner.jsx
@@ -1,10 +1,15 @@
 import './styles/Banner.css';
+import image1 from '../assets/oxana-v-qoAIlAmLJBU-unsplash.jpg'
 
 
 function Banner() {
     return (
         <section className="welcome-banner">
-          blablbalabalalabballa
+          <div> <img src={image1} alt='travel journal'/> </div>
+          <div>
+            <h2>Welcome to Travel tales</h2>
+            <p>your ultimate travel companion! our app is designed to capture and enhance every moment of your journey.share experiences with friends, and discover hidden gems along the way.</p>
+          </div>
         </section>
     )
 }
