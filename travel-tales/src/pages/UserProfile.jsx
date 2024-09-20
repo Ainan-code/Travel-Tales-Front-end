b9627c8f969350137d25e2e634f5caf0d2c838f9
AINAN HASSAN <ainanhassan123@gmail.com>
2024-09-20 19:43:01 +0300

    user profile card made

--- travel-tales/src/pages/UserProfile.jsx
+++ travel-tales/src/pages/UserProfile.jsx
@@ -3,20 +3,23 @@ import Diary from "../components/diary";
 import "../components/styles/Profile.css";
 function UserProfile() {
     return(
-        <div className="user-container">
-            <div className="profile-card">
-                <div className="profile-image">
-                    <img src={profileimg} alt="profile" />
-                </div>
-                <div className="username">
-                    ALI ABDI
-                </div>
-            </div>
-           <div className="user-post">
-                <Diary/>
-            
-            </div>
+    <div className="user-profile">
+        <div className="card">
+        <div className="cover-photo">
+            <img src="https://i.imgur.com/KykRUCV.jpeg" className="profile"/>
         </div>
+        <h3 className="profile-name">James Carson</h3>
+        <p className="about">UI/UX Designer  Front End Developer</p>
+        <button className="btn">Message</button>
+        <button className="btn">Following</button>
+    
+        </div>
+        <div className="user-post">
+            <h4>Your post</h4>
+          <Diary/>
+        </div>
+   </div>
+       
     )
 }
 
