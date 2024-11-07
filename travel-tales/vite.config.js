8596881f9f993b7379e1f30a6af44fbd07ea38a9
Ainan-code <ainanhassan123@gmail.com>
2024-11-07 23:25:06 +0300

    yes

--- travel-tales/vite.config.js
+++ travel-tales/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Travel-Tales-Front-end/",
+  base: "/Travel-Tales-Front-end/travel-tales/",
   plugins: [react()],
 })
